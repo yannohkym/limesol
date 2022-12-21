cb745920690e1b094c0be10b6d9a561ba9218d87
yannohkym <45738341+yannohkym@users.noreply.github.com>
2022-12-21 13:49:47 +0300

    now

--- app/Http/Controllers/EntityController.php
+++ app/Http/Controllers/EntityController.php
@@ -13,20 +13,25 @@ class EntityController extends Controller
         $client = new Client();
         $response = $client->get('https://api.publicapis.org/entries');
         $entries = json_decode($response->getBody());
-        dd($entries);
-        foreach ($entries as $entry){
-            $data = new EntityOne();
-            $data->api =$entry->API;
-            $data->description =$entry->Description;
-            $data->auth =$entry->Auth;
-            $data->https =$entry->HTTPS;
-            $data->cors =$entry->Cors;
-            $data->link =$entry->Link;
-            $data->category =$entry->Category;
-            $data->save();
+        $third = floor(count($entries) / 3);
+        $thirds = array_chunk($entries, $third);
+        foreach ($entries as $entri){
+            foreach ($entri as $entry){
+
+                $data = new EntityOne();
+                $data->api =$entry->API;
+                $data->description =$entry->Description;
+                $data->auth =$entry->Auth;
+                $data->https =$entry->HTTPS;
+                $data->cors =$entry->Cors;
+                $data->link =$entry->Link;
+                $data->category =$entry->Category;
+                $data->save();
+            }
         }
 
-return 'Entries from the  api were saved';
+
+        return 'Entries from the  api were saved';
 
 
 
