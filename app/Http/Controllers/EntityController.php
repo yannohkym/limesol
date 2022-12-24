fe9e56b90f584d60f9965db63926abf3e8b2c253
yannohkym <45738341+yannohkym@users.noreply.github.com>
2022-12-24 09:27:28 +0300

    now

--- app/Http/Controllers/EntityController.php
+++ app/Http/Controllers/EntityController.php
@@ -13,29 +13,29 @@ class EntityController extends Controller
         $client = new Client();
         $response = $client->get('https://api.publicapis.org/entries');
         $entries = json_decode($response->getBody());
-        $third = floor(count($entries) / 3);
-        $thirds = array_chunk($entries, $third);
-        foreach ($entries as $entri){
-            foreach ($entri as $entry){
+        //dd($entries->entries[0]);
+        $array = $entries->entries;
+        $chunks = array_chunk($array, floor(count($array) / 3));
+       // dd($chunks);
 
+        foreach ($chunks as $i => $items) {
+            foreach ($items as $item) {
+                // create a new model instance
                 $data = new EntityOne();
-                $data->api =$entry->API;
-                $data->description =$entry->Description;
-                $data->auth =$entry->Auth;
-                $data->https =$entry->HTTPS;
-                $data->cors =$entry->Cors;
-                $data->link =$entry->Link;
-                $data->category =$entry->Category;
+                $data->api =$item->API;
+                $data->description =$item->Description;
+                $data->auth =$item->Auth;
+                $data->https =$item->HTTPS;
+                $data->cors =$item->Cors;
+                $data->link =$item->Link;
+                $data->category =$item->Category;
+                // set the table name based on the chunk number
+                //$data->setTable('entity_ones_table' . ($i + 1));
                 $data->save();
             }
         }
 
-
         return 'Entries from the  api were saved';
-
-
-
-
     }
     public function saveToEntity1(){
 
